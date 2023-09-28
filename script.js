364ef32b05ff00eac119e5f3f0fdd7a11e28898c
Adam Sharp <adam.sharp2003@gmail.com>
2023-09-28 15:42:27 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let input = document.getElementById("input"),
+timeDisplay = document.getElementById("time"),
+bestTimeDisplay = document.getElementById("bestTime"),
+lettersTable = document.getElementById("letters-table"), 
+expectedText = "abcdefghijklmnopqrstuvwxyz",
+bestTimesArray = JSON.parse(localStorage.getItem("bestTimesArray")) || [], currentIndex = 0,
+calculateTotal = (obj) => Object.values(obj).reduce((acc, val) => acc + parseFloat(val), 0).toFixed(2),
+letterTimes = bestTimesArray[0] || {}, previousTime = 0.00, startTime, timerInterval
+
+function updateTimer() {
+	timeDisplay.textContent = ((Date.now() - startTime) / 1000).toFixed(2);
+}
+
+input.addEventListener("input", function (event) {
+	if (!startTime) startTime = Date.now(), timerInterval = setInterval(updateTimer, 10);
+	if (input.value === expectedText.slice(0, input.value.length) && event.data != null) {
+		letterTimes[expectedText[currentIndex]] = ((Date.now() - startTime - previousTime) / 1000).toFixed(2);
+		previousTime = Date.now() - startTime;
+	} else input.value = expectedText.slice(0, currentIndex);
+	if (input.value === expectedText) {
+		input.disabled = true
+		clearInterval(timerInterval);
+		timeDisplay.textContent = ((Date.now() - startTime) / 1000).toFixed(2);	
+		bestTimesArray.push(letterTimes)
+		localStorage.setItem("bestTimesArray", JSON.stringify(bestTimesArray.sort((a, b) => calculateTotal(a) - calculateTotal(b))));
+		displayLetterTimes();
+		bestTimeDisplay.textContent = calculateTotal(bestTimesArray[0])
+	}
+	currentIndex = input.value.length;
+});
+
+function displayLetterTimes() {
+	while (lettersTable.firstChild) lettersTable.removeChild(lettersTable.firstChild);
+	var bestTimes = bestTimesArray.reduce((result, obj) => Object.assign(result, ...Object.keys(obj).map(key => ({[key]: Math.min(result[key] || Infinity, parseFloat(obj[key]))}))),{});
+	lettersTable.innerHTML += `<tr><th>Letters</th><th>Times (s)</th><th>Best Possible Times (s)</th></tr>
+	${Object.keys(Object.fromEntries(Object.entries(letterTimes).sort())).map(letter => `<tr>
+		<td>${letter}</td>
+		<td style="background-color: rgb(${77*(letterTimes[letter]/Math.max(...Object.values(letterTimes)))+34},34,34)">${letterTimes[letter]+(letterTimes[letter]==bestTimes[letter]?'*':'')}</td>
+		<td style="background-color: rgb(${77*(bestTimes[letter]/Math.max(...Object.values(bestTimes)))+34},34,34)">${bestTimes[letter]}</td>
+	</tr>`).join('')}
+	<tr><th>Total</th><th>${calculateTotal(letterTimes)}</th><th>${calculateTotal(bestTimes)}</th></tr>`;
+}
+
+document.getElementById("reset-button").addEventListener("click", function () {
+	input.value = "";
+	input.disabled = false;
+	input.focus();
+	clearInterval(timerInterval);
+	startTime = null;
+	timeDisplay.textContent = "0.00";
+	letterTimes = {};
+	previousTime = 0;
+	currentIndex = 0
+});
+
+window.onload = () => {
+	input.onpaste = e => e.preventDefault();
+}
+
+if (bestTimesArray.length > 0) bestTimeDisplay.textContent = calculateTotal(bestTimesArray[0])
+displayLetterTimes()
